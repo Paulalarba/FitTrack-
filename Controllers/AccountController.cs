2d0c648654a0cf06556fb9512e3bf2a49839d1a3
Paul alarba <alarbapaulax@gmail.com>
2026-04-12 17:20:18 +0800

    Landing/Login/SignUp page added not functional so far

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using FitTrack.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitTrack.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+         public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignUp(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Implement registration logic (save to DB)
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitTrack.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Commander name is required")]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Neural sync email is required")]
+        [EmailAddress(ErrorMessage = "Invalid neural link address")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Access key is required")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Access key must be at least 8 characters")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please select a primary directive")]
+        public string Directive { get; set; } // MuscleGain, WeightLoss, Maintenance
+    }
+}
